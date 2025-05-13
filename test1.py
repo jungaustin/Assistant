baecc4062afec58b75a027d08b91a3f3f6a2b705
AusJung <137535372+jungaustin@users.noreply.github.com>
2025-05-12 17:49:38 -0700

    Removed tracking of test files and added them to .gitignore

--- test1.py
+++
@@ -1,4 +0,0 @@
-from spotify_client import SpotifyClient
-sc = SpotifyClient()
-sc.get_device_id()
-sc.shuffle(True)
--- testing.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def play_music(song_name= None, artist =None, playlist=None):
-    subprocess.Popen(["open", "-a", "Spotify"])
-
-play_music()
